1bb1def8851d517fb582880fdca6668c203d230d
Bobby Powers <bobbypowers@gmail.com>
2012-02-06 08:37:17 -0500

    start of type resolution

--- pass_type_resolution.go
+++ pass_type_resolution.go
@@ -8,10 +8,18 @@ import (
 
 func passTypeResolution(f *File) {
 	var curr Node
-	typeScope := make([]*Scope, 2)
-	objScope := make([]*Scope, 2)
+	typeScope := make([]*Scope, 0, 2)
+	objScope := make([]*Scope, 0, 2)
 	depth := 0
 	var currModel *ModelDecl
+
+	currScope := func() *Scope {
+		last := len(objScope) - 1
+		if last == -1 {
+			return nil
+		}
+		return objScope[last]
+	}
 
 	Inspect(f, func(node Node) bool {
 		// inspect is called with nil at the end of a production
@@ -35,6 +43,10 @@ func passTypeResolution(f *File) {
 			fmt.Println(strings.Repeat("  ", depth) + n.Name())
 		case *AssignStmt:
 			fmt.Println(strings.Repeat("  ", depth) + n.Name())
+		case *RefExpr:
+			if currScope().Lookup(n.Name) == nil {
+				panic("unknown variable referenced: " + n.Name)
+			}
 		}
 		fmt.Printf("%s%d (%T)\n", strings.Repeat("  ", depth), depth, node)
 		//fmt.Printf("%s%#v\n", indent, node)
