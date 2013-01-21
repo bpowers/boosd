0553170174e8c6a9f61f4d44336f30d427da5052
Bobby Powers <bobbypowers@gmail.com>
2013-01-21 15:12:48 -0500

    runtime: curr and next maps

--- runtime/base.go
+++ runtime/base.go
@@ -4,8 +4,26 @@ import (
 	"fmt"
 )
 
+type Timespec struct {
+	Start    float64
+	End      float64
+	DT       float64
+	SaveStep float64
+}
+
+// TODO: define useful methods on table
+type Table [2][]float64
+
 type BaseSim struct {
-	Parent Model
+	Parent   Model
+	Timespec Timespec
+
+	Tables map[string]Table
+	Curr   map[string]float64
+	Next   map[string]float64
+	// TODO: eventually we should pull constants out into their
+	// own map
+	//Constants map[string]float64
 }
 
 func (s *BaseSim) Model() Model {
@@ -31,7 +49,10 @@ func (s *BaseSim) SetValue(name string, val float64) error {
 	return nil
 }
 
-type BaseModel struct{}
+type BaseModel struct {
+	Tables    map[string]Table
+	Constants map[string]float64
+}
 
 func (m *BaseModel) Attr(name string) interface{} {
 	return nil
