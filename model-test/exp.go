2c472ed3861b10c2f8ba5b00fc20fd035c94572a
Bobby Powers <bobbypowers@gmail.com>
2013-01-21 14:12:52 -0500

    runtime: work on flushing out Model interface

--- model-test/exp.go
+++ model-test/exp.go
@@ -2,12 +2,55 @@ package main
 
 import (
 	"boosd/runtime"
-	"log"
+	"fmt"
 )
 
+type simMain struct{}
+
+func (s *simMain) Model() runtime.Model {
+	return nil
+}
+
+func (s *simMain) RunTo(t float64) error {
+	return fmt.Errorf("not implemented")
+}
+
+func (s *simMain) RunToEnd() {
+}
+
+func (s *simMain) GetValue(name string) float64 {
+	return -1
+}
+func (s *simMain) GetValueSeries(name string) [][2]float64 {
+	return nil
+}
+
+func (s *simMain) SetValue(name string, val float64) error {
+	return nil
+}
+
+type mdlMain struct{}
+
+func (m *mdlMain) NewSim() runtime.Sim {
+	return &simMain{}
+}
+
+func (m *mdlMain) Attr(name string) interface{} {
+	return nil
+}
+
+func (m *mdlMain) VarNames() []string {
+	return nil
+}
+
+func (m *mdlMain) VarInfo(name string) map[string]interface{} {
+	return nil
+}
+
+func init() {
+	runtime.Register(new(mdlMain))
+}
+
 func main() {
-	var err error
-	if err = runtime.Init(); err != nil {
-		log.Fatalf("runtime.Init(): %s", err)
-	}
+	runtime.Main()
 }
--- runtime/runtime.go
+++ runtime/runtime.go
@@ -1,5 +1,46 @@
 package runtime
 
-func Init() error {
-	return nil
+import (
+	"log"
+)
+
+var models = make([]Model, 0, 1)
+
+type Sim interface {
+	Model() Model
+
+	RunTo(t float64) error
+	RunToEnd()
+
+	GetValue(name string) float64
+	GetValueSeries(name string) [][2]float64
+
+	SetValue(name string, val float64) error
+}
+
+type Model interface {
+	NewSim() Sim
+	Attr(name string) interface{}
+	VarNames() []string
+	VarInfo(name string) map[string]interface{}
+}
+
+func Register(ms ...Model) {
+	models = append(models, ms...)
+}
+
+// Init initializes the boosd runtime.
+func Main() {
+	// FIXME: this should be temporary
+	if len(models) != 1 {
+		log.Fatalf("len(%v) != 1", models)
+	}
+
+	m := models[0]
+
+	sim := m.NewSim()
+
+	sim.RunToEnd()
+
+	// TODO: print results
 }
